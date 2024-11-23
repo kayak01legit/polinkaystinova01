ca6e077843b2eeb1fad3d137cb2f3c2101f8863f
kayak01legit <kayak01legit@icloud.com>
2024-11-23 10:58:58 +0300

    Updated resources

--- resources/setup.sh
+++ resources/setup.sh
@@ -2,3 +2,4 @@ Virtual Machines Load Testing Act as if what you do makes a difference. It does.
 Machine Learning I find that the harder I work, the more luck I seem to have. WebSockets Parallel Processing Microservices
 JWT To be the best, you must be able to handle the worst. NLP Server Response Containerization I find that the harder I work, the more luck I seem to have. Event Sourcing Logging
 Zero Downtime Deployment Content Delivery Network Deployment Cache Invalidation Testing Backend If you can dream it, you can do it. Kafka Logging GraphQL Simplicity is the ultimate sophistication. Multi-threading Scalability Microservices
+Data Science Scalability CI/CD IoT Event-driven Architecture Event Sourcing Machine Learning Lambda Functions I find that the harder I work, the more luck I seem to have.
